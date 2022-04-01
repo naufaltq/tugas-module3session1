84243d439a3f71fd2654191d8a05bbfaf1a41da0
Naufal Taufiqurrahman <naufaltq16@gmail.com>
2022-04-01 16:07:34 +0700

    homework module 3 session 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import data from "./contents/data";
+import Home from "./components/Pages/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">{data}</header>
+      <Home />
     </div>
   );
 }
